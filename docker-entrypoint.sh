ecde4816a73a78423ae961a5589d80f0ae80bff3
Julian Haupt <julian.haupt@hauptmedia.de>
2015-03-27 11:53:24 +0100

    changed entrypoint so it will not only work with a linked mysql container but also with the specified credentials

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
 
-if [ -z "$MYSQL_NAME" ]; then
-	echo "Missing linked mysql container"
-	exit 1
+if [ -n "$MYSQL_NAME" ]; then
+	MYSQL_USER=root
+	MYSQL_PASSWORD=${MYSQL_ENV_MYSQL_ROOT_PASSWORD}
+	MYSQL_HOST_ADDR=${MYSQL_PORT_3306_TCP_ADDR}
+	MYSQL_HOST_PORT=${MYSQL_PORT_3306_TCP_PORT}
 fi
 
 cat >/etc/dbconfig-common/phpmyadmin.conf <<EOF
@@ -11,10 +13,10 @@ dbc_install='true'
 dbc_upgrade='true'
 dbc_remove='true'
 dbc_dbtype='mysql'
-dbc_dbuser='root'
-dbc_dbpass='${MYSQL_ENV_MYSQL_ROOT_PASSWORD}'
-dbc_dbserver='${MYSQL_PORT_3306_TCP_ADDR}'
-dbc_dbport='${MYSQL_PORT_3306_TCP_PORT}'
+dbc_dbuser='${MYSQL_USER}'
+dbc_dbpass='${MYSQL_PASSWORD}'
+dbc_dbserver='${MYSQL_HOST_ADDR}'
+dbc_dbport='${MYSQL_HOST_PORT}'
 dbc_dbname=''
 dbc_dbadmin='root'
 dbc_basepath=''
